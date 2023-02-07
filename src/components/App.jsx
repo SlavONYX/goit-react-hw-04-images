686648b3e6a16f49a6e6fe8d98783c6809e81c6f
SlavONYX <slavonixxx@gmail.com>
2023-02-07 10:37:01 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import {  useState,useEffect } from 'react'
 import { ImageGallery } from "./ImageGallery/ImageGallery";
 import { Searchbar } from "./Searchbar/Searchbar";
 import { Button } from './Button/Button';
